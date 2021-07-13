d06641400159fd645c2a1b6d4d66bc6cc82f918a
MehediHasanRaj <mehedihasanraj007@gmail.com>
2021-07-14 05:09:56 +0600

    raj

---
+++ A2OJ Ladder/Codeforces rating 1300/test.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define fastIO  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define loop(i,a,b) for(int i =a; i < b; ++i)
+
+using namespace std;
+
+void solve(){
+    cout << "this is checking" << endl;
+}
+
+int main(){
+    fastIO;
+    int t = 1; cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
